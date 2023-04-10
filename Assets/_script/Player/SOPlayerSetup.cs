51b15188dc6f6d881db59066bd287f9571bb8a23
Helio Junior <theancientguardian@protonmail.com>
2023-04-10 10:36:17 -0300

    (Atenção) Aula 4 mod 17
    Fiz alguma mudança que está dando erro no script, vou fazer o commit dessas mudanças e depois reverter para aula anterior para ver se concerto.

---
+++ Assets/_script/Player/SOPlayerSetup.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+[CreateAssetMenu]
+public class SOPlayerSetup : ScriptableObject
+{
+    [Header("Speed Setup")]
+    public Vector2 friction = new Vector2(.1f, 0);
+    public Vector2 velocity;
+    public float speed;
+    public float speedRun;
+    public float forceJump;
+    public Ease ease = Ease.OutBack;
+
+
+    [Header("Animation Setup")]
+    public float JumpScaleY = 1.5f;
+    public float JumpScaleX = .7f;
+    public float AnimationDuration = .3f;
+    
+
+    [Header("Animation player")]
+    public string boolRun = "Run";
+    public string triggerDeath = "Death";
+    public float playerSwipeDuration = .1f;
+}
